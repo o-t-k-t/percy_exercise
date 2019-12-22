9f4f9e55e18454ea2849a1da518714fa878ceda9
o-t-k-t <wetcondition@gmail.com>
2019-12-22 18:46:38 +0900

    仮タスクページ

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Amplify from 'aws-amplify'
+
+const aws_exports = require('./aws-exports.js');
+const components = require('aws-amplify-vue');
 
 Vue.config.productionTip = false;
 
+Amplify.configure(aws_exports);
+
+/* eslint-disable no-new */
 new Vue({
   router,
   store,
